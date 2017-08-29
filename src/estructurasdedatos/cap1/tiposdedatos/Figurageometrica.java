fff41af97af995334cc0bf69e17587fda32f9182
JoseArciniegas <30849534+JoseArciniegas@users.noreply.github.com>
2017-08-28 21:50:40 -0500

    Create Figurageometrica.java

---
+++ src/estructurasdedatos/cap1/tiposdedatos/Figurageometrica.java
@@ -0,0 +1,37 @@
+package estructuradedatos.cap1.tiposdedatos;
+
+public class Figurageometrica{
+	private String figura;
+  	private String tipo;
+  	private String nombre;
+
+  public int crearFigura(){return 0;}
+  public int calcularArea(){return 0;}
+  	
+  	public String getFigura(){
+  		return figura;
+    }
+      
+    public void setFigura(String figura){
+      	this.cedula = figura;
+    }
+  	
+  public String getTipo(){
+  		return tipo;
+  }
+
+  public String setTipo(String tipo){
+  	this.nombre = tipo;
+  }
+
+  public String getNombre(){
+  		return nombre;
+  }
+
+  public String setNombre(String nombre){
+  	this.nombre = nombre;
+  }
+
+    
+  }
+}
