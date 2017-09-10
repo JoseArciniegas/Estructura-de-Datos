b6954d0d41435ed5eecc9a98a42be8b2670bb53b
JoseArciniegas <30849534+JoseArciniegas@users.noreply.github.com>
2017-09-09 22:13:46 -0500

    Subiendo codigo del vector.

--- src/estructurasdedatos/cap2/arreglos/Vector.java
+++ src/estructurasdedatos/cap2/arreglos/Vector.java
@@ -9,6 +9,58 @@ package estructurasdedatos.cap2.arreglos;
  *
  * @author tusk
  */
-public class Vector {
-    
+public class Vector{
+  private int elementos;
+  private int vector;
+}
+
+public void Vector(int nelementos){
+  this.nelementos = nelementos;
+  this.Vector = new int [nelementos];
+}
+
+public void Insertar (int e, int dato){
+  if(this.nelementos<elementos){
+    this.matriz[e] = dato;
+    else
+    System.out.println("error");
+  }
+}
+
+public void Listar(){
+  for(int i = 0 ; i <= this.elementos ; i++){
+    System.out.println(this.elementos);
+  }
+}
+
+public int Buscar(nelementos){
+  int contador;
+  for(int i = 0 ; i < nelementos ; i++){
+    for(int j=i ; j < nelementos ; j++){
+      if(nelementos[i]<nelementos[j]){
+        contador = nelementos[i];
+        nelementos[i]=nelementos[j];
+        nelementos[j]=contador;
+      }
+  }
+}
+}
+
+static boolean Busqueda(int[] vector, int dato){
+  for (int i = 0 ; i < vector ; i++){
+    if (vector[i] == dato){
+      return true;
+    }
+  }
+  return false;
+}
+
+public void Actualizar(){
+  if(this.nelementos<elementos){
+    this.vector[e] = nuevodato;
+  }
+  }
+
+public Borrar(elementos){
+  this.vector(elementos) = null;
 }
