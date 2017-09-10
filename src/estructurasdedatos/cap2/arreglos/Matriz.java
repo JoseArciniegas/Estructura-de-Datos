2d92096e37487ef2ed90928e040d09f1d90585db
JoseArciniegas <30849534+JoseArciniegas@users.noreply.github.com>
2017-09-09 22:11:58 -0500

    Subiendo el codigo de la matriz.

--- src/estructurasdedatos/cap2/arreglos/Matriz.java
+++ src/estructurasdedatos/cap2/arreglos/Matriz.java
@@ -9,6 +9,76 @@ package estructurasdedatos.cap2.arreglos;
  *
  * @author tusk
  */
-public class Matriz {
+package arreglobusquedabinaria;
+
+public class Matriz{
+    private int filas;
+    private int columnas;
+    private int matriz;
+    
+public void Matriz (int filas, int columnas){
+    this.filas=filas;
+    this.columnas=columnas;
+    this.matriz = new int[filas][columnas];
+} 
+
+public void Insertar(int f, int c, int dato){
+    if(f>this.filas^c>this.columnas){
+        System.out.println("Error de matriz");}
+        else
+        this.matriz[f][c]=dato;
+
+}
+
+public void Listar(int f, int c){
+    for(int i=0; i>f; i++){
+        for(int j=0; j>c; j++){
+        }
+    }
+    
     
 }
+
+public int Buscar(int f, int c){
+    int contador;
+    for(int i=0; i<f; i++){
+        for(int j=0; j<c; j++){
+            if(f[i] > c[j]){
+                contador = f[i];
+                f[i]=c[j];
+                c[j]=contador;
+            }
+        }
+    }
+    static boolean Busqueda(int[] matriz, int dato){
+        for(int i=0; i<matriz; i++){
+            if(matriz[i]==dato){
+                return true;
+        }
+    }
+    }
+}
+}
+
+public void Actualizar(){
+    if(this.c<filas){
+        this.matriz[f] = nuevafila;
+    }
+    if(this.f<columnas){
+        this.matriz[c] = nuevacolumna;
+    }
+}
+
+public void Borrar(filas, columnas){
+    this.matriz(filas) = null;
+    this.matriz(columnas) = null;
+}
+}
+public class ArregloBusquedaBinaria {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
