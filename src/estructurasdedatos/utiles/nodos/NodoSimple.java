2ae5c5992d77bac6f21280a83c196ad431aecec6
JoseArciniegas <30849534+JoseArciniegas@users.noreply.github.com>
2017-09-17 13:41:38 -0500

    Subiendo la lista Enlazada Simple

--- src/estructurasdedatos/utiles/nodos/NodoSimple.java
+++ src/estructurasdedatos/utiles/nodos/NodoSimple.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package estructurasdedatos.utiles.nodos;
+package listaenlazada;
 
-/**
- *
- * @author tusk
- */
-public class NodoSimple {
+public class ListaEnlazada {
+    Nodo cabeza;
     
+    ListaEnlazada(){
+        cabeza=null;
+    }
+    
+    public boolean ListaVacia(){
+        return(cabeza==null)? true: false; //verifica si la lista esta vacia para insertar un nuevo dato.
+    }
+    
+    public void Insertar(int dato){
+        Nodo nuevo=new Nodo(dato); //creamos el nodo.
+        if(ListaVacia()){
+            cabeza=nuevo;
+        }
+        else{
+            nuevo.siguiente=cabeza;
+            cabeza=nuevo;
+            
+        }
+    }
+    
+    public String Listar(){
+        String s="Lista\n";
+        Nodo aux=cabeza;
+        
+        while(aux!=null){
+            s+=aux.dato+"\n";
+            aux=aux.siguiente;
+        }
+        
+    }
+    
+    public void Borrar(cabeza){
+       this.Nodo(cabeza)=null; 
+    }
+    
+
+    public static void main(String[] args) {
+        
+    }
 }
