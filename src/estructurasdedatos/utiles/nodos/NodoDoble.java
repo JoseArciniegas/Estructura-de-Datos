1008bca1f2bd9828e4301e9d02bc0a7e4b05061f
JoseArciniegas <arciniegas2012@utp.edu.cogit config --globalgit config --global user.email arciniegas2012@utp.edu.co>
2017-09-20 00:28:55 -0500

    Subiendo el nodo Doble creando metodos de insertar cabeza y cola

--- src/estructurasdedatos/utiles/nodos/NodoDoble.java
+++ src/estructurasdedatos/utiles/nodos/NodoDoble.java
@@ -10,5 +10,35 @@ package estructurasdedatos.utiles.nodos;
  * @author tusk
  */
 public class NodoDoble {
+    private Nodo cabeza;
+    private Nodo cola;
+    
+    public NodoDoble(){
+        cabeza=null;
+        cola=null;
+    }
+    
+    public void insertarCabeza(int dato){
+        if(cabeza == null){
+            cabeza=new Nodo(dato, null, null);
+            cola = cabeza;
+        }else{
+            Nodo nuevo = new Nodo(dato, null, cabeza);
+            cabeza.setAnterior(nuevo);
+            cabeza = nuevo;
+        }
+        
+    }
+    
+    public void insertarCola(int dato){
+        if(cabeza==null){
+            cola=new Nodo(dato,null,null);
+            cabeza=cola;
+        }else{
+            Nodo nuevo = new Nodo(dato, null, cabeza);
+            cola.setAnterior(nuevo);
+            cola=nuevo;
+        }
+    }
     
 }
