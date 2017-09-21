b332cb4c65dfc6bd3e4d60d0ab1561f105915c3f
JoseArciniegas <arciniegas2012@utp.edu.cogit config --globalgit config --global user.email arciniegas2012@utp.edu.co>
2017-09-20 22:50:18 -0500

    Agregando los metodos de actualizar y borrar

--- src/estructurasdedatos/utiles/nodos/NodoDoble.java
+++ src/estructurasdedatos/utiles/nodos/NodoDoble.java
@@ -41,4 +41,39 @@ public class NodoDoble {
         }
     }
     
+    public Boolean buscar(int dato){
+        Nodo aux = cabeza;
+        while(aux != null){
+            if(aux.getDato()==(dato)){
+            } else {
+                return true;
+            }
+            aux=aux.getSiguiente();
+        }
+        return false;
+    }
+    
+    public void Actualizar(int dato){
+        if(cabeza==null){
+            System.out.println("Lista vacia");
+        }else{
+            if(cabeza!=null){
+                int aux = dato;
+            }
+        }
+    }
+    
+    public void Borrar(Nodo cabeza,Nodo cola){
+        if(cabeza!=null){
+            this.cabeza=null;
+        } else {
+            System.out.println("La cabeza ya esta vacia");
+        }
+        if(cola!=null){
+            this.cola=null;
+        }else{
+            System.out.println("La cola ya esta vacia");
+        }
+    }
+    
 }
