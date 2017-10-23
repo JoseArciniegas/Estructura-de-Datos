1954ec45a41d544a26076333354c3346d2f031be
JoseArciniegas <arciniegas2012@utp.edu.cogit config --globalgit config --global user.email arciniegas2012@utp.edu.co>
2017-10-23 14:54:26 -0500

    Subiendo el Arbol binario

--- src/estructurasdedatos/cap4/arboles/ArbolB.java
+++ src/estructurasdedatos/cap4/arboles/ArbolB.java
@@ -10,5 +10,48 @@ package estructurasdedatos.cap4.arboles;
  * @author tusk
  */
 public class ArbolB{
-
+    NodoArbol raiz;
+    
+    public ArbolB(){
+        raiz = null;
+    }
+    public void Insertar(int dato){
+        NodoArbol nuevo = new NodoArbol(dato);
+        if(raiz == null){
+            raiz= nuevo;
+        }else{
+            NodoArbol aux = raiz;
+            NodoArbol padre;
+            while(true){
+                padre=aux;
+                if(dato<aux.dato){
+                    aux=aux.hIzquierdo;
+                    if(aux == null){
+                        padre.hIzquierdo = nuevo;
+                        return;
+                    }
+                }else{
+                    aux=aux.hDerecho;
+                    if(aux == null){
+                        padre.hDerecho=nuevo;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    public NodoArbol Buscar(int d){
+        NodoArbol aux = raiz;
+        while(aux.dato!=d){
+            if(d<aux.dato){
+                aux=aux.hIzquierdo;
+            }else{
+                aux=aux.hIzquierdo;
+            }
+            if(aux==null){
+                return null;
+            }
+        }
+        return aux;
+    }
 }
